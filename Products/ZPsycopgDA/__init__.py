bd32c5b5c79444f3cc7b5feff77f31bf43ad14f4
Nicola Gramola <n.gramola@divigroup.com>
2021-03-05 16:44:32 +0100

    Fix version

--- Products/ZPsycopgDA/__init__.py
+++ Products/ZPsycopgDA/__init__.py
@@ -16,7 +16,7 @@
 # their work without bothering about the module dependencies.
 
 __doc__ = "ZPsycopg Database Adapter Registration."
-__version__ = '2.4.7.dev1+perfact.4'
+__version__ = '2.4.7.dev1+n1k9.1'
 
 # Python2 backward compatibility
 try:
